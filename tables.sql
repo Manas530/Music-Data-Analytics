b4178e3dce8b0e0c9aaf0f0ec1f66bd765b5e471
Manas530 <manasbadhan@gmail.com>
2025-03-13 19:12:07 +0530

    Add files via upload

---
+++ tables.sql
@@ -0,0 +1,14 @@
+/*total 11 tables present in MusiconlineDB database;*/
+
+select * from customers;
+select * from employee;
+select * from artist;
+select * from genre;
+select * from invoice;
+select * from track;
+select * from playlist;
+select * from playlist_track;
+select * from media_type;
+select * from invoice_line;
+select * from album;
+
